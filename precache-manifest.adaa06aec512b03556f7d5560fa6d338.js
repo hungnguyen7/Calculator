e2cb232ad775fd83e5f12ad6186b4f59cffe9759
hungnguyen7 <hungnguyenst.99@gmail.com>
2020-07-31 11:25:45 +0700

    Updates

---
+++ precache-manifest.adaa06aec512b03556f7d5560fa6d338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05e50fa2eab5ddf5d5f7193d471d33f",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "64e6caf6a6660268fc98",
+    "url": "/calculator/static/css/main.7292b56d.chunk.css"
+  },
+  {
+    "revision": "2e20946ac0981b464cb9",
+    "url": "/calculator/static/js/2.aaa56ad1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.aaa56ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e6caf6a6660268fc98",
+    "url": "/calculator/static/js/main.6388911b.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  },
+  {
+    "revision": "ff9074e1cb8c8590b22961dc96f7ecbf",
+    "url": "/calculator/static/media/imgBackground.ff9074e1.jpg"
+  }
+]);
